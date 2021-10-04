39bcd34899e5e2656d2a330b2901302fd558e322
HoussemBL <lahmarhoussem90@gmail.com>
2021-10-04 08:57:23 +0200

    Update DeltaProcessing.scala

--- src/main/scala/com/datalake/DeltaProcessing.scala
+++ src/main/scala/com/datalake/DeltaProcessing.scala
@@ -6,7 +6,6 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql._
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.functions.lit
-
 import org.apache.log4j.{Level, Logger}
 
 import Utils._
